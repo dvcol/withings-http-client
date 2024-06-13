b66b7ad87f03c5bbe5fb381f707f7c62fca822d6
Dinh-Van Colomban <van.colomban@gmail.com>
2024-06-13 20:05:05 +0200

    chore(deps): moves to common utils

--- lib/api/endpoints/sleep.endpoint.ts
+++ lib/api/endpoints/sleep.endpoint.ts
@@ -1,4 +1,4 @@
-import { HttpMethod } from '@dvcol/base-http-client/utils/http';
+import { HttpMethod } from '@dvcol/common-utils/http';
 
 import type { WithingsSleepGetRequest, WithingsSleepGetResponse, WithingsSleepSummaryRequest } from '~/models/withings-sleep.model';
 
--- lib/api/endpoints/stetho.endpoint.ts
+++ lib/api/endpoints/stetho.endpoint.ts
@@ -1,4 +1,4 @@
-import { HttpMethod } from '@dvcol/base-http-client/utils/http';
+import { HttpMethod } from '@dvcol/common-utils/http';
 
 import type { WithingsStetho, WithingsStethoListRequest, WithingsStethoListResponse, WithingsStethoRequest } from '~/models/withings-stetho.model';
 
